7387713896eaaa8067b764eb2d30d0ec2e3b6374
Thomas <tomsclater@live.com>
2022-08-13 02:38:14 -0700

    Creating functions

--- main.go
+++ main.go
@@ -6,6 +6,30 @@ import(
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "PraseForm(" err: "w", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name =%s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET"{
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+}
+
 func main(){
 	fileServer := http.FileServer(http.Dir("/static"))
 	http.Handle("/", fileServer)
